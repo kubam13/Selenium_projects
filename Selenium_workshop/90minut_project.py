b576f3c0071872e1456c674f272d41c5bd058d6c
Kuba <kubam1327@gmail.com>
2024-06-11 13:22:59 +0200

    SQL/Python connection first try

--- Selenium_workshop/90minut_project.py
+++ Selenium_workshop/90minut_project.py
@@ -12,4 +12,9 @@ cookies_ok = driver.find_element(By.CLASS_NAME, 'css-2evfjs')
 cookies_ok.click()
 transfers = driver.find_element(By.XPATH, '/html/body/table[2]/tbody/tr[1]/td[1]/table/tbody/tr[4]/td/div[8]/a')
 transfers.click()
+players_green = driver.find_elements(By.CLASS_NAME, 'tr_green')
+players_list = []
+for p in range(len(players_green)):
+    players_list.append(players_green[p].text)
+print(players_list)
 time.sleep(5)
